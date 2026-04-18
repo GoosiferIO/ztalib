4aa35142cb7fa5fbaf6e5d523a581102ac735431
Eric Galvan <lericgalvan@outlook.com>
2026-04-18 15:30:22 -0700

    ApeCore -> ApeF files

---
+++ src/parsers/ApeF.h
@@ -0,0 +1,80 @@
+#ifndef APECORE_H
+#define APECORE_H
+
+// APE.CORE - ZT1 Graphics Parser
+// by Eric Galvan (Goosifer.IO)
+// https://github.com/openztcc/APE.Core
+// Licensed under MIT (see LICENSE)
+//
+// This library parses ZT1 game sprites and animations. Please feel
+// free to use in your own projects and tools.
+//
+// Credits to Jeff Bostoen for his fantastic documentation on the ZT1 file formats:
+// https://github.com/jbostoen/ZTStudio/wiki/ZT1-Graphics-Explained
+//
+// Credits to the original author of the STB image library (for PNG writing):
+// - Sean Barrett (released under public domain/MIT)
+// 
+// Version 0.6.3
+
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+
+#include "./include/stb_image_write.h"
+#include "src/data/ApePixelBlock.h"
+#include "src/data/ApePixelSet.h"
+#include "src/data/ApeFrame.h"
+#include "src/data/ApeInfo.h"
+#include "src/data/ApeColor.h"
+#include "src/data/ApeFrameBuffer.h"
+#include "src/parsers/PalF.h"
+
+#define MAGIC "FATZ"
+#define APE_CORE_VERSION "0.6.4"
+
+// if FATZ is first 4 bytes, additional 5 bytes ahead
+// The ninth byte is a boolean value that specifies if there is an 
+// a background frame 
+
+
+// -------------------------------- Standard Pixel Output
+
+class ApeF
+{
+    public: 
+        ApeF();
+        virtual ~ApeF();
+
+        int load(std::string fileName, int colorProfile = 0, std::string ioPal = "");
+        int save(std::string fileName);
+        int exportToPNG(std::string fileName, ApeFrameBuffer output);
+        int getFrameCount();
+        ApeFrameBuffer** getFrameBuffers();
+        std::string getPalLocation();
+        static int validateGraphicFile(std::string fileName);
+        int hasBackgroundFrame();
+        // return header info
+        static ApeInfo getHeader(std::string fileName);
+
+    private:
+        static int hasMagic(std::ifstream &input);
+        int writeBuffer();
+        // binary input
+        std::ifstream input;
+        // output buffers
+        ApeFrameBuffer** frameBuffers;
+        // data
+        ApeInfo info;
+        std::vector<ApeFrame> frames;
+        std::vector<std::vector<ApePixelBlock>> pixelBlocks;
+        PalF palFile;
+        // other
+        bool hasBackground = false;
+        int colorModel;
+        std::string palLocation;
+    };
+
+#endif // APECORE_H

3122b679f7773c94a08d1fe11d73a760514ab94a
Eric Galvan <lericgalvan@outlook.com>
2026-04-20 18:43:37 -0700

    Add remaining include guards

--- src/data/ApeInfo.h
+++ src/data/ApeInfo.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef APEINFO_H
+#define APEINFO_H
+
 #include <vector>
 
 struct ApeInfo
@@ -6,3 +8,5 @@ struct ApeInfo
     uint32_t speed; // animation speed in ms
     uint32_t frameCount;
 };
+
+#endif // APEINFO_H

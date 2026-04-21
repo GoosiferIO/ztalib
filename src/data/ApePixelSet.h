499d02bf796c2d7fe8a579d5d13e1b00b6d1778b
Eric Galvan <lericgalvan@outlook.com>
2026-04-20 21:01:31 -0700

    `blocks` in `ApePixelSet` no longer vector of pointers

--- src/data/ApePixelSet.h
+++ src/data/ApePixelSet.h
@@ -6,7 +6,7 @@
 struct ApePixelSet 
 {
     uint8_t blockCount; // How many pixel blocks
-    std::vector<std::unique_ptr<ApePixelBlock>> blocks; // The pixel blocks
+    std::vector<ApePixelBlock> blocks; // The pixel blocks
 };
 
 #endif // APEPIXELSET_H

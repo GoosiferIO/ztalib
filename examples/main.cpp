46865e9657dce2e8e2b36fb6ed4596d5f67ed477
Eric Galvan <lericgalvan@outlook.com>
2026-04-20 22:54:01 -0700

    Update main.cpp to reflect recent changes

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,19 +1,20 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../include/apespeed.h"
+// #include "../include/apespeed.h"
 #include <string>
 #include <iostream>
 // #include <chrono>
 // #include <omp.h>
+#include "../src/parsers/ApeF.h"
 
 int main(int argc, char const *argv[])
 {
     // Validate arguments
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " <input_file> <palette_file> [num_threads] [output_name]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <input_file> <palette_file> [output_name]" << std::endl;
         return 1;
     }
 
-    ApeCore core;
+    ApeF apef;
     std::string input_file = argv[1];
     std::string palette_file = argv[2];
     int num_threads = 1;
@@ -22,17 +23,17 @@ int main(int argc, char const *argv[])
     std::cout << "Using palette " << palette_file << std::endl;
 
     // set number of threads
-    if (num_threads >= 4) {
-        num_threads = std::stoi(argv[3]);
-        std::cout << "Number of threads set to " << num_threads << std::endl;
-    } else {
-        std::cout << "Number of threads set to 1" << std::endl;
-        num_threads = 1;
-    }
+    // if (num_threads >= 4) {
+    //     num_threads = std::stoi(argv[3]);
+    //     std::cout << "Number of threads set to " << num_threads << std::endl;
+    // } else {
+    //     std::cout << "Number of threads set to 1" << std::endl;
+    //     num_threads = 1;
+    // }
 
     // set output name
-    if (argc >= 5) {
-        output_name = argv[4];
+    if (argc >= 4) {
+        output_name = argv[3];
         std::cout << "Output name set to " << output_name << std::endl;
     } else {
         std::cout << "Output name set to default" << std::endl;
@@ -44,7 +45,7 @@ int main(int argc, char const *argv[])
     // double total_time = 0.0;
     // auto start = std::chrono::high_resolution_clock::now();
 
-    if (!core.load(input_file, 0, palette_file)) {
+    if (!apef.load(input_file, 0, palette_file)) {
         std::cerr << "Failed to load file: " << input_file << std::endl;
         return 1;
     }
@@ -53,21 +54,21 @@ int main(int argc, char const *argv[])
 
     // std::chrono::duration<double> runtime = end - start;
     // total_time += runtime.count();
-    // std::cout << "Time taken to decode " << core.getFrameCount() << " frames: " << total_time << " seconds" << std::endl;
-    // std::cout << "Average time per frame: " << total_time / core.getFrameCount() << " seconds" << std::endl;
+    // std::cout << "Time taken to decode " << apef.getFrameCount() << " frames: " << total_time << " seconds" << std::endl;
+    // std::cout << "Average time per frame: " << total_time / apef.getFrameCount() << " seconds" << std::endl;
 
     // NOTE: UNCOMMENT THIS TO WRITE PNGs
-    // int numBuffers = core.getFrameCount();
-    // std::cout << "Writing PNGs" << std::endl;
-    // int successCount = 0;
-    // for (int i = 0; i < numBuffers; i++) {
-    //     int error = core.exportToPNG(output_name + std::to_string(i) + ".png", *core.apeBuffer()[i]);
-    //     if (error < 0) {
-    //         std::cout << "Failed to write frame " << i << " to png" << std::endl;
-    //     } else {
-    //         successCount++;
-    //     }
-    // }
+    int numBuffers = apef.getFrameCount();
+    std::cout << "Writing PNGs" << std::endl;
+    int successCount = 0;
+    for (int i = 0; i < numBuffers; i++) {
+        int error = apef.exportToPng(output_name + std::to_string(i) + ".png", apef.getFrameBuffer()[i]);
+        if (error < 0) {
+            std::cout << "Failed to write frame " << i << " to png" << std::endl;
+        } else {
+            successCount++;
+        }
+    }
 
     // std::cout << "Finished writing " << successCount << " frames to PNG with " << numBuffers - successCount << " errors." << std::endl;
     

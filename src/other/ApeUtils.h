1b94af6eace51d365e3dbdf90fe2330ffe455ed6
Eric Galvan <lericgalvan@outlook.com>
2026-04-20 23:07:36 -0700

    Create `ApeUtils.cpp` and move fun defs

--- src/other/ApeUtils.h
+++ src/other/ApeUtils.h
@@ -11,34 +11,4 @@ public:
     static int hasMagic(std::ifstream &);
 };
 
-int ApeUtils::hasMagic(std::ifstream &_file)
-{
-    char magic[5] = {0};
-    _file.read(magic, 4);
-
-    // std::cout << "\tMagic Bytes: " << magic << std::endl;
-
-    // read at least 4 bytes
-    // if less than 4 bytes, not FATZ
-    if (_file.gcount() < 4)
-    {
-        _file.clear();
-        _file.seekg(0, std::ios::beg);
-        return 0;
-    }
-
-    // test for FATZ
-    if (strcmp(magic, MAGIC) != 0)
-    {
-        _file.clear();
-        _file.seekg(0, std::ios::beg);
-        return 0;
-    }
-
-    // FATZ found
-    _file.clear();
-    _file.seekg(0, std::ios::beg);
-    return 1;
-}
-
 #endif // APEUTILS_H

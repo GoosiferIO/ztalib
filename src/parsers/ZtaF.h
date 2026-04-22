ddd07ff15d22e09f916cf2cd524fa038b3feb0c3
Eric Galvan <lericgalvan@outlook.com>
2026-04-22 14:23:01 -0700

    Add licensing and preamble to ZtaF.cpp

--- src/parsers/ZtaF.h
+++ src/parsers/ZtaF.h
@@ -1,21 +1,25 @@
 #ifndef ZTAF_H
 #define ZTAF_H
 
-// ape-graphics-parser - ZT1 Graphics Parser
-// by Eric Galvan (Goosifer.IO)
-// https://github.com/openztcc/ZTA.Core
-// Licensed under MIT (see LICENSE)
-//
-// This library parses ZT1 game sprites and animations. Please feel
-// free to use in your own projects and tools.
-//
-// Credits to Jeff Bostoen for his fantastic documentation on the ZT1 file formats:
-// https://github.com/jbostoen/ZTStudio/wiki/ZT1-Graphics-Explained
-//
-// Credits to the original author of the STB image library (for PNG writing):
-// - Sean Barrett (released under public domain/MIT)
-//
-// Version 0.6.3
+/* ZtaF.h -- parser for zt1 animation files
+
+    ztalib - ZT1 graphics parser
+    https://goosifer.io/
+    Copyright 2025 Eric Galvan (GoosiferIO)
+
+    OFFICIAL REPOSITORY
+
+        https://github.com/GoosiferIO/ztalib
+
+    LICENSE
+
+        MIT License (see LICENSE at end of file or at project root)
+
+    ACKNOWLEDGMENTS
+
+        Thank you to Jeff Bostoen for his documentation on the ZT1 file formats:
+        https://github.com/jbostoen/ZTStudio/wiki/ZT1-Graphics-Explained
+*/
 
 #include <fstream>
 #include <vector>
@@ -67,3 +71,27 @@ private:
 };
 
 #endif // ZTAF_H
+
+/*
+    MIT License
+
+    Copyright (c) 2025 Goosifer.IO
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/

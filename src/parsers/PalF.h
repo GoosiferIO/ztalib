2ac4329c89e2ced3a425ea0e93b89b9ff3e0e095
Eric Galvan <35750901+GoosiferIO@users.noreply.github.com>
2026-04-23 03:31:45 -0700

    [Chore] Remove undefined `PalF::load` (#35)
    * Remove `PalF::load`
    
    * Refactor source

--- src/parsers/PalF.h
+++ src/parsers/PalF.h
@@ -30,7 +30,6 @@ class PalF
 {
 public:
     PalF();
-    int load(std::string fileName);
     virtual ~PalF();
     std::string location;
     uint32_t nameSize;
@@ -48,7 +47,7 @@ private:
 
 /*
     MIT License
-
+ 
     Copyright (c) 2025 Eric Galvan (GoosiferIO)
 
     Permission is hereby granted, free of charge, to any person obtaining a copy

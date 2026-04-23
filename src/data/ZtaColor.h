6d25fefbb9a750120d89b74154aa8e87bdd73712
Eric Galvan <lericgalvan@outlook.com>
2026-04-22 20:29:46 -0700

    License and preample for all /data files

--- src/data/ZtaColor.h
+++ src/data/ZtaColor.h
@@ -5,8 +5,24 @@
 #include "ZtaPixelBlock.h"
 #include <cstdint>
 
-/*
-Struct that contains color information for a single pixel in the PAL file.
+/* ZtaColor.h -- struct for color information in ZT1 palette files
+
+    ztalib - ZT1 graphics parser
+    https://goosifer.io/
+    Copyright 2025 Eric Galvan (GoosiferIO)
+
+    OFFICIAL REPOSITORY
+
+        https://github.com/GoosiferIO/ztalib
+
+    LICENSE
+
+        MIT License (see LICENSE at end of file or at project root)
+
+    ACKNOWLEDGMENTS
+
+        Thank you to Jeff Bostoen for his documentation on the ZT1 file formats:
+        https://github.com/jbostoen/ZTStudio/wiki/ZT1-Graphics-Explained
 */
 
 struct ZtaColor 
@@ -18,3 +34,27 @@ struct ZtaColor
 };
 
 #endif // ZTACOLOR_H
+
+/*
+    MIT License
+
+    Copyright (c) 2025 Eric Galvan (GoosiferIO)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/

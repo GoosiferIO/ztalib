ae3dd2cb12eda64071a3f568caeb24c567660084
Eric Galvan <lericgalvan@outlook.com>
2026-04-25 19:00:31 -0700

    Add additional bindings

--- bindings/pyzta.cpp
+++ bindings/pyzta.cpp
@@ -33,9 +33,36 @@ PYBIND11_MODULE(pyzta, m) {
         .def("speed", &ZtaInfo::speed)
         .def("frame_count", &ZtaInfo::frameCount);
 
+    py::class_<ZtaFrameBufferObject>(m, "ZtaFrameBufferObject")
+        .def_readonly("pixels", &ZtaFrameBufferObject::pixels)
+        .def("width", &ZtaFrameBufferObject::width)
+        .def("height", &ZtaFrameBufferObject::height)
+        .def("offset_x", &ZtaFrameBufferObject::offsetX)
+        .def("offset_y", &ZtaFrameBufferObject::offsetY)
+        .def("channels", &ZtaFrameBufferObject::channels);
+
+    py::class_<ZtaFrameBuffer>(m, "ZtaFrameBuffer")
+        .def_readonly("frames", &ZtaFrameBuffer::frames);
+
+    py::class_<PalF, std::shared_ptr<PalF>>(m, "PalF")
+        .def(py::init<>())
+        .def("location", &PalF::location)
+        .def("set_location", &PalF::location)
+        .def("location_size", &PalF::locationSize)
+        .def("set_location_size", &PalF::locationSize)
+        .def("color_model", &PalF::colorModel)
+        .def("set_color_model", &PalF::colorModel)
+        .def("num_colors", &PalF::numColors)
+        .def("set_num_colors", &PalF::numColors)
+        .def("get_color", &PalF::getColor)
+        .def("load", &PalF::load);
+
     py::class_<ZtaF>(m, "ZtaF")
         .def(py::init<>())
-        .def("load", &ZtaF::load, py::arg("file_name"), py::arg("color_profile") = 0, py::arg("io_pal") = "")
+        .def("load", &ZtaF::load, 
+            py::arg("file_name"), 
+            py::arg("color_profile") = 0, 
+            py::arg("io_pal") = "")
         .def("data", &ZtaF::data)
         .def("get_frame_buffer", &ZtaF::getFrameBuffer);
 } 
